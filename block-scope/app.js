67635319d4b77669be3da14a12fccfd531fd0e3e
davidjaguilar104 <78775228+davidjaguilar104@users.noreply.github.com>
2021-03-25 20:33:03 -0500

    heap and stack

---
+++ block-scope/app.js
@@ -0,0 +1,25 @@
+// let name = 'Max';
+
+// if (name === 'Max') {
+//     let hobbies = ['Sports', 'Cooking']; 
+//     console.log(hobbies); 
+// }
+
+// function greet() {
+//     let age = 30; 
+//     let name = 'Manuel'; 
+//     console.log(name, age, hobbies); 
+// }
+
+// console.log(name, hobbies); 
+
+// greet(); 
+'use strict'; 
+
+
+const userName = 'Max';
+var undefined = 5; // to show that using the strict mode on line 17 disables this 
+
+
+
+console.log(userName);
